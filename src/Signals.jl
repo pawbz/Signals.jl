acc2707019c1883b549f45eeedde6564cb5b7cfb
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2018-04-24 12:31:20 -0400

    addnoise

--- src/Signals.jl
+++ src/Signals.jl
@@ -3,4 +3,39 @@ module Signals
 include("DSP.jl")
 include("Wavelets.jl")
 
+
+"""
+
+Output xN after adding noise to x
+
+SNR:: in decibel scale
+"""
+function addnoise!(xN, x, SNR)
+
+
+	# store zero mean version of x in xN
+	xavg=0.0
+	for i in eachindex(x)
+		xavg += x[i]
+	end
+	xavg /= length(x)
+
+	for i in eachindex(x)
+		xN[i] = x[i] - xavg
+	end
+
+	σx=var(xN)
+
+	σxN=sqrt(σx^2*inv(10^(SNR/10.)))
+	
+	# factor to be multiplied to each scalar
+	α=sqrt(σxN)
+	for i in eachindex(x)
+		xN[i] = x[i] + α*randn()
+	end
+end
+
+
+
+
 end # module
